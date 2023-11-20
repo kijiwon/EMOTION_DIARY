c04b116fed4d66c365ce3d4196be3e8e1360e0b6
kijiwon <kiji1313@gmail.com>
2023-11-20 22:10:53 +0900

    Diary:상세 페이지 구현 완료

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -18,6 +18,7 @@ const Edit = () => {
       if (targetDiary) {
         setOriginData(targetDiary);
       } else {
+        alert("존재하지 않는 일기입니다");
         navigate("/", { replace: true });
       }
     }
