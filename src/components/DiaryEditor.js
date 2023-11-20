cd09805949baf2481cec10922f73832c0c1d67fa
kijiwon <kiji1313@gmail.com>
2023-11-20 16:57:40 +0900

    New: New 컴포넌트 구현 완료

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,16 +1,82 @@
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
-
+import EmotionItem from "./EmotionItem";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef, useContext } from "react";
+import { DiaryDispatchContext } from "./../App.js";
 
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion1.png`,
+    emotion_descript: "완전 좋음",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion2.png`,
+    emotion_descript: "좋음",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion3.png`,
+    emotion_descript: "그럭저럭",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion4.png`,
+    emotion_descript: "나쁨",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `assets/emotion5.png`,
+    emotion_descript: "끔찍함",
+  },
+];
+
+// const getStringDate = (date) => {
+//   return date.toISOString().slice(0, 10);
+// };
+
+export const getStringDate = (date) => {
+  let year = date.getFullYear();
+
+  let month = date.getMonth() + 1;
+
+  let day = date.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  return `${year}-${month}-${day}`;
 };
 
 const DiaryEditor = () => {
   const navigate = useNavigate();
+  const contentRef = useRef();
+  const { onCreate } = useContext(DiaryDispatchContext);
+
   const [date, setDate] = useState(getStringDate(new Date()));
+  const [emotion, setEmotion] = useState(3);
+  const [content, setContent] = useState("");
+
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
+
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(date, content, emotion);
+    navigate("/", { replace: true });
+    // 뒤로가기를 통해 작성 페이지로 오지 못하도록 replace 옵션을 true로 설정
+  };
 
   return (
     <div className="DiaryEditor">
@@ -29,6 +95,40 @@ const DiaryEditor = () => {
               value={date}
               onChange={(e) => setDate(e.target.value)}
               type="date"
+            />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요"
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+        </section>
+        <section>
+          <div className="control_box">
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
+            <MyButton
+              text={"작성완료"}
+              type={"positive"}
+              onClick={handleSubmit}
             />
           </div>
         </section>
